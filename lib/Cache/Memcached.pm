a5995e9050579cd981104becaacef41d6799c226
dormando <dormando@b0b603af-a30f-0410-a34e-baf09ae79d0b>
2010-06-18 01:26:15 +0000

    Checking in changes prior to tagging of version 1.29.
    Changelog diff is:
    
    Index: ChangeLog
    ===================================================================
    --- ChangeLog	(revision 843)
    +++ ChangeLog	(working copy)
    @@ -1,3 +1,5 @@
    +2010-06-17: version 1.29
    +
         * fix warnings spew if calling stats on a down server
     
         * fix buck2sock confusion (Eddie Canales)
    
    
    git-svn-id: http://code.sixapart.com/svn/memcached/trunk/api/perl@844 b0b603af-a30f-0410-a34e-baf09ae79d0b

--- lib/Cache/Memcached.pm
+++ lib/Cache/Memcached.pm
@@ -37,7 +37,7 @@ use constant F_COMPRESS => 2;
 use constant COMPRESS_SAVINGS => 0.20; # percent
 
 use vars qw($VERSION $HAVE_ZLIB $FLAG_NOSIGNAL $HAVE_SOCKET6);
-$VERSION = "1.28";
+$VERSION = "1.29";
 
 BEGIN {
     $HAVE_ZLIB = eval "use Compress::Zlib (); 1;";
